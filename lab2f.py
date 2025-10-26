94a28c569b9663b6690cc708611b23bc25bb4401
Shikshya Sharma <ssharma608@myseneca.ca>
2025-10-26 19:21:03 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+#Author: Shikshya Sharma
+#Author ID: ssharma608
+
+
+#Date Created:2025/09/21
+
+import sys 
+
+arguments = sys.argv
+
+count = int(sys.argv[1])
+
+
+while count > 0:
+    print (str(count))
+    count = count - 1
+
+
+
+print("blast off!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
