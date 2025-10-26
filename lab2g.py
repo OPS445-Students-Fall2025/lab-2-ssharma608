6b98218502d801a580f6c6ffc393f249890e1795
Shikshya Sharma <ssharma608@myseneca.ca>
2025-10-26 19:22:55 -0400

    Added lab2

---
+++ lab2g.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+#Author: Shikshya Sharma
+#Author ID: ssharma608
+
+
+#Date Created:2025/09/21
+
+import sys 
+
+arguments = sys.argv
+if len(sys.argv) == 1:
+    timer = 3
+else:
+    timer = int(sys.argv[1])
+while timer > 0:
+    print (str(timer))
+    timer = timer - 1
+    
+
+
+print("blast off!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
